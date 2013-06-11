ffb385f1000fac233b11624c911b1b350cbd2c76
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-11 17:24:04 -0500

    refactored with object properties

--- public/js/game.js
+++ public/js/game.js
@@ -5,60 +5,55 @@
         count = 0;
     }
 
-    Game.prototype.incrementCounter = function () {
-        count++;
-    };
+    Game.prototype = {
+        incrementCounter: function () {
+            count++;
+        },
 
-    Game.prototype.startTimers = function () {
-        if (count === 1) {
-            start = new Date();
-        }
-    };
+        startTimers: function () {
+            if (count === 1) {
+                start = new Date();
+            }
+        },
 
-    Game.prototype.stopTimers = function () {
-        end = new Date();
-        race_time = (end - start) / 1000;
-        return race_time;
-    };
+        stopTimers: function () {
+            end = new Date();
+            race_time = (end - start) / 1000;
+            return race_time;
+        },
 
-    Game.prototype.declareWinner = function (player) {
-        if (player === this.player1){
-          $('.winner h2').text(player.initials()+" ,you're a Speedy Foxzales!" + race_time + ' seconds!' );
+        declareWinner: function (player) {
+            if (player === this.player1) {
+              shoutOut = ", you're a Speedy Foxzales! ";
+            } else if (player === this.player2) {
+              shoutOut = " is a flying Squirrel! ";
+            }
+          $('.winner h2').text(player.initials() + shoutOut + race_time + ' seconds!');
           $('.winner').show();
-        }
-        else if (player === this.player2){
-          $('.winner h2').text(player.initials()+" is a flying Squirrel! " + race_time + ' seconds!' );
-          $('.winner').show();
-        }
-    };
+        },
 
-     Game.prototype.movePlayer = function (player, key) {
-        player.move(key);
+        movePlayer: function (player, key) {
+            player.move(key);
             if (player.checkLocation(key) === "winner") {
                 this.stopTimers();
                 this.declareWinner(player);
             }
+        },
+
+        onKeyUp: function (key) {
+            this.incrementCounter();
+            this.startTimers();
+
+            if (key === 80) {
+                this.movePlayer(this.player1, key);
+
+            } else if (key === 81) {
+                this.movePlayer(this.player2, key);
+            }
+        }
     };
 
-
-    Game.prototype.onKeyUp = function (key) {
-        this.incrementCounter();
-        this.startTimers();
-
-        if (key === 80) {
-        this.movePlayer(this.player1, key);
-
-    } else if (key === 81) {
-      this.movePlayer(this.player2, key);
-    }
-};
-
-window.Game = Game;
+    window.Game = Game;
 
 })(window);
-
-
-
-
- 
 
